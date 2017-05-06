e5b49c11b1fbb18d981689b59799b81dc0d3d6a3
Ben Kochie <bjk@gitlab.com>
2017-05-06 10:04:29 +0200

    Add Prometheus metrics.

--- src/main/readout.go
+++ src/main/readout.go
@@ -172,6 +172,69 @@ func publishReadouts(c *smartpi.Config, mqttclient MQTT.Client, values [25]float
 	}
 }
 
+const metricsNamespace = "smartpi"
+
+var (
+	currentMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: metricsNamespace,
+			Name:      "amps",
+			Help:      "Line current",
+		},
+		[]string{"phase"},
+	)
+	voltageMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: metricsNamespace,
+			Name:      "volts",
+			Help:      "Line voltage",
+		},
+		[]string{"phase"},
+	)
+	activePowerMetirc = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: metricsNamespace,
+			Name:      "active_watts",
+			Help:      "Active Watts",
+		},
+		[]string{"phase"},
+	)
+	cosphiMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: metricsNamespace,
+			Name:      "phase_angle",
+			Help:      "Line voltage phase angle",
+		},
+		[]string{"phase"},
+	)
+	frequencyMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: metricsNamespace,
+			Name:      "phase_frequency_hertz",
+			Help:      "Line frequency in hertz",
+		},
+		[]string{"phase"},
+	)
+)
+func updatePrometheusMetrics(v [25]float32) {
+	currentMetric.WithLabelValues("A").Set(float64(v[0]))
+	currentMetric.WithLabelValues("B").Set(float64(v[1]))
+	currentMetric.WithLabelValues("C").Set(float64(v[2]))
+	currentMetric.WithLabelValues("N").Set(float64(v[3]))
+	voltageMetric.WithLabelValues("A").Set(float64(v[4]))
+	voltageMetric.WithLabelValues("B").Set(float64(v[5]))
+	voltageMetric.WithLabelValues("C").Set(float64(v[6]))
+	activePowerMetirc.WithLabelValues("A").Set(float64(v[7]))
+	activePowerMetirc.WithLabelValues("B").Set(float64(v[8]))
+	activePowerMetirc.WithLabelValues("C").Set(float64(v[9]))
+	cosphiMetric.WithLabelValues("A").Set(float64(v[10]))
+	cosphiMetric.WithLabelValues("B").Set(float64(v[11]))
+	cosphiMetric.WithLabelValues("C").Set(float64(v[12]))
+	frequencyMetric.WithLabelValues("A").Set(float64(v[13]))
+	frequencyMetric.WithLabelValues("B").Set(float64(v[14]))
+	frequencyMetric.WithLabelValues("C").Set(float64(v[15]))
+}
+
 func pollSmartPi(config *smartpi.Config, device *i2c.Device) {
 	var mqttclient MQTT.Client
 
@@ -205,8 +268,11 @@ func pollSmartPi(config *smartpi.Config, device *i2c.Device) {
 
 			writeSharedFile(config, valuesr)
 
-			//Publish readouts to MQTT
+			// Publish readouts to MQTT.
 			publishReadouts(config, mqttclient, valuesr)
+
+			// Update metrics endpoint.
+			updatePrometheusMetrics(valuesr)
 
 			for index, _ := range data {
 				switch index {
@@ -254,6 +320,12 @@ func init() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.DebugLevel)
+
+	prometheus.MustRegister(currentMetric)
+	prometheus.MustRegister(voltageMetric)
+	prometheus.MustRegister(activePowerMetirc)
+	prometheus.MustRegister(cosphiMetric)
+	prometheus.MustRegister(frequencyMetric)
 	prometheus.MustRegister(version.NewCollector("smartpi"))
 }
 
