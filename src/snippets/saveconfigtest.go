f86e32b8b051d2937ea50507bd883a1199ca72f9
Ben Kochie <superq@gmail.com>
2017-12-06 16:28:55 +0100

    Apply go fmt.

--- src/snippets/saveconfigtest.go
+++ src/snippets/saveconfigtest.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-  "github.com/nDenerserve/SmartPi/src/smartpi"
+	"github.com/nDenerserve/SmartPi/src/smartpi"
 )
 
 func main() {
 
 	config := smartpi.NewConfig()
-  fmt.Println(config.FTPpath)
-  config.FTPpath = "smartpi/"
-  fmt.Println(config.FTPpath)
-  config.SaveParameterToFile()
+	fmt.Println(config.FTPpath)
+	config.FTPpath = "smartpi/"
+	fmt.Println(config.FTPpath)
+	config.SaveParameterToFile()
 }
